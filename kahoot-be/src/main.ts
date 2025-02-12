7b56ccb50966017bcad16fc558353a393e8ddd27
Nguyen Ngoc Anh Tuan <Tomtepfa@gmail.com>
2025-02-12 15:54:18 +0700

    fix/cors: enable cors for api

--- kahoot-be/src/main.ts
+++ kahoot-be/src/main.ts
@@ -1,6 +1,6 @@
 import { AllExceptionsFilter } from '@base/filters/all-exceptions.filter';
 import { ResponseInterceptor } from '@base/interceptors/response.interceptor';
-import { GLOBAL_PREFIX } from '@constants';
+import { corsConfig, GLOBAL_PREFIX } from '@constants';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
@@ -13,6 +13,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix(GLOBAL_PREFIX);
 
+  app.enableCors(corsConfig);
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.useGlobalFilters(new AllExceptionsFilter());
   app.useGlobalPipes(
