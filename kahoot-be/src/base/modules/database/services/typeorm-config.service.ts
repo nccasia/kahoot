6ffc6b19097034a156ef11a903ce5edb9d4376c3
Nguyen Ngoc Anh Tuan <Tomtepfa@gmail.com>
2025-02-28 16:51:45 +0700

    fix/database: disable auto sync database schema on production

--- kahoot-be/src/base/modules/database/services/typeorm-config.service.ts
+++ kahoot-be/src/base/modules/database/services/typeorm-config.service.ts
@@ -24,7 +24,7 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
       username: this.configService.getOrThrow<string>('POSTGRES_USER'),
       password: this.configService.getOrThrow<string>('POSTGRES_PASSWORD'),
       database: this.configService.getOrThrow<string>('POSTGRES_DATABASE'),
-      synchronize: true,
+      synchronize: this.configService.get('NODE_ENV') !== 'production',
       entities: [
         User,
         Game,
